52f4a08acc872abea6705428922dc4a6b578e808
tnraro <deokji53@tnraro.com>
2026-09-13 11:43:40 +0900

    Interactable 컴포넌트에 네트워크 기능 추가

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,15 +1,42 @@
+using AIWalk.Networking;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Interactable : MonoBehaviour
 {
+  [Header("Events")]
   public UnityEvent OnInteract;
   public UnityEvent OnCursorEnter;
   public UnityEvent OnCursorExit;
 
+  [Header("Network")]
+  [SerializeField]
+  private NetworkIdentity networkIdentity;
+
+
+  NetworkClient networkClient;
+  string interactEventName;
+  bool isSubscribed = false;
+
+  void Start()
+  {
+    networkClient = NetworkClient.Instance;
+    InitializeNetwork();
+    SubscribeNetworkEvent();
+  }
+  void OnEnable()
+  {
+    SubscribeNetworkEvent();
+  }
+  void OnDisable()
+  {
+    UnsubscribeNetworkEvent();
+  }
+
   public void Interact()
   {
     OnInteract.Invoke();
+    SendNetworkInteractEvent();
   }
 
   public void EnterCursor()
@@ -32,5 +59,49 @@ public class Interactable : MonoBehaviour
     {
       child.gameObject.layer = layer;
     }
+  }
+
+  void InitializeNetwork()
+  {
+    if (networkIdentity == null) return;
+    if (networkClient == null)
+    {
+      Debug.LogError($"[{nameof(Interactable)}] NetworkClient is not available.", this);
+      return;
+    }
+    if (string.IsNullOrEmpty(networkIdentity.Id))
+    {
+      Debug.LogError($"[{nameof(Interactable)}] NetworkIdentity.Id is empty.", this);
+      return;
+    }
+
+    interactEventName = networkIdentity.Id + "/interact";
+  }
+
+  void SubscribeNetworkEvent()
+  {
+    if (isSubscribed || networkClient == null || string.IsNullOrEmpty(interactEventName)) return;
+    Debug.Log("[SUB] " + interactEventName, this);
+    networkClient.Subscribe(interactEventName, OnNetworkInteract);
+    isSubscribed = true;
+  }
+  void UnsubscribeNetworkEvent()
+  {
+    if (!isSubscribed || networkClient == null || string.IsNullOrEmpty(interactEventName)) return;
+    Debug.Log("[UNSUB] " + interactEventName, this);
+    networkClient.Unsubscribe(interactEventName, OnNetworkInteract);
+    isSubscribed = false;
+  }
+  void SendNetworkInteractEvent()
+  {
+    if (networkClient == null || string.IsNullOrEmpty(interactEventName)) return;
+    Debug.Log("[SEND] " + interactEventName, this);
+    networkClient.SendEvent(interactEventName, EventAudience.Others, "");
+  }
+
+  void OnNetworkInteract(GameBackendEvent _)
+  {
+    Debug.Log("[RECV] " + interactEventName, this);
+    OnInteract.Invoke();
   }
 }
