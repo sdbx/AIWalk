205f7fe4594abde978eba6e8550c72f43945c331
tnraro <deokji53@tnraro.com>
2026-09-18 21:12:53 +0900

    DoorLock에 NetBehaviour 적용

--- Assets/Scripts/DoorLock.cs
+++ Assets/Scripts/DoorLock.cs
@@ -1,28 +1,82 @@
+using System;
+using AIWalk.Networking;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class DoorLock : MonoBehaviour
+public class DoorLock : NetBehaviour
 {
   public UnityEvent OnUnlocked;
   public UnityEvent OnInvalid;
 
-
   public bool locked = true;
 
-  [SerializeField]
-  private NetworkController networkController;
   [SerializeField]
   private string password;
 
   public void Unlock(string inputPassword)
   {
-    if (password != inputPassword)
+    if (IsHost)
     {
-      networkController.SendEvent("OnInvalid");
-      return;
-    }
-    locked = false;
+      if (!ValidPassword(inputPassword))
+      {
+        SendEvent("doorlock.invalid", EventAudience.All, "");
+        return;
+      }
 
-    networkController.SendEvent("OnUnlocked");
+      SendEvent("doorlock.unlocked", EventAudience.All, "");
+    }
+    else
+    {
+      SendEvent("doorlock.unlock", EventAudience.Host, new UnlockData(inputPassword));
+    }
+  }
+
+  bool ValidPassword(string inputPassword)
+  {
+    return IsHost && password == inputPassword;
+  }
+
+  protected override void OnNetworkReady()
+  {
+    if (IsHost)
+    {
+      Subscribe("doorlock.unlock", e =>
+      {
+        var data = e.DeserializePayload<UnlockData>();
+        Debug.Log($"password: {data.password}");
+
+        Unlock(data.password);
+      });
+    }
+
+    Subscribe("doorlock.unlocked", _ =>
+    {
+      _OnUnlocked();
+    });
+    Subscribe("doorlock.invalid", _ =>
+    {
+      _OnInvalid();
+    });
+  }
+
+  void _OnUnlocked()
+  {
+    OnUnlocked.Invoke();
+    locked = false;
+  }
+  void _OnInvalid()
+  {
+    OnInvalid.Invoke();
+  }
+
+  [Serializable]
+  struct UnlockData
+  {
+    public string password;
+
+    public UnlockData(string password)
+    {
+      this.password = password;
+    }
   }
 }
